70515d09b2bb3abd7ed4d38772037a98826ef142
lyf_laptop <luoyifan902008@126.com>
2025-04-02 08:28:34 +0800

    更改G1通信方式

--- setup_laptop.sh
+++ setup_laptop.sh
@@ -7,11 +7,11 @@ source $SCRIPT_DIR/unitree_ros2_ws/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 
 # LAN
-# export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-#                             <NetworkInterface name="enp0s31f6" priority="default" multicast="default" />
-#                         </Interfaces></General></Domain></CycloneDDS>'
-
-# WLAN
 export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
-                            <NetworkInterface name="wlp0s20f3" priority="default" multicast="default" />
+                            <NetworkInterface name="enp0s31f6" priority="default" multicast="default" />
                         </Interfaces></General></Domain></CycloneDDS>'
+
+# # WLAN
+# export CYCLONEDDS_URI='<CycloneDDS><Domain><General><Interfaces>
+#                             <NetworkInterface name="wlp0s20f3" priority="default" multicast="default" />
+#                         </Interfaces></General></Domain></CycloneDDS>'
